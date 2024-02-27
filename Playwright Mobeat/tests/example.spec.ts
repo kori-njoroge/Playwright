1c1ada50dce8dad03a271aedad6fcac3649e35b3
Kori Njoroge <korinjoroge63@gmail.com>
2024-02-27 22:48:49 +0300

    run tests in debug mode and traces

--- Playwright Mobeat/tests/example.spec.ts
+++ Playwright Mobeat/tests/example.spec.ts
@@ -13,6 +13,9 @@ test('get started link', async ({ page }) => {
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
+  // 
+  await expect(page).toHaveURL(/.*intro/);
+
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });

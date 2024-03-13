e31f9848eb0f8a47e5471974d216deaf5564211c
Kori Njoroge <korinjoroge63@gmail.com>
2024-03-13 14:06:50 +0300

    alternative playwright waits

--- pw-practice-app/tests/autowaiting.spec.ts
+++ pw-practice-app/tests/autowaiting.spec.ts
@@ -17,3 +17,19 @@ test.beforeEach(async ({page}) => {
 
         await expect(successBtn).toHaveText('Data loaded with AJAX get request.',{timeout: 20000})
     })
+
+    test('alternative waits', async({page})=>{
+        const successBtn = page.locator('.bg-success')
+
+        // --Wait for element
+        // await page.waitForSelector('.bg-success')
+
+        // --wait for particcular reponse
+        // await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+
+        // --wait for network calls to be completed (NOT RECOMMENDED)
+        await page.waitForLoadState('networkidle')
+
+        const text = await successBtn.allTextContents()
+        expect(text).toContain('Data loaded with AJAX get request.')
+    })

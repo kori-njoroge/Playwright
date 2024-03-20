bc4c70c130fd1cbab5697480c832f8806a964353
Kori Njoroge <korinjoroge63@gmail.com>
2024-03-20 14:56:52 +0300

    Moving sliders simulating mouse movements

--- pw-practice-app/tests/uiComponents.spec.ts
+++ pw-practice-app/tests/uiComponents.spec.ts
@@ -204,5 +204,18 @@ test("sliders", async ({ page }) => {
     })
     await tempGauge.click()
 
+    // Simulate  drag event - mouse movement
+    const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger')
+    await tempBox.scrollIntoViewIfNeeded()
 
+    const box = await tempBox.boundingBox()
+    const x = box.x + box.width / 2
+    const y = box.y + box.height / 2
+    await page.mouse.move(x, y)
+    await page.mouse.down()
+    await page.mouse.move(x + 100, y)
+    await page.mouse.move(x+100, y + 100)
+    await page.mouse.up()
+
+    await expect(tempBox).toContainText('30')
 })

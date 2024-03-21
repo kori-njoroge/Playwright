01073f348361e4ac510fc10e7bd75770bfe972fc
Kori Njoroge <korinjoroge63@gmail.com>
2024-03-21 15:46:53 +0300

    Reverting the changes to Keep It Simple Stupid (KISS)

--- pw-practice-app/page-objects/navigatioPage.ts
+++ pw-practice-app/page-objects/navigatioPage.ts
@@ -1,45 +1,35 @@
-import { Locator, Page } from "@playwright/test";
+import { Page } from "@playwright/test";
 
 export class NavigatioPage {
     readonly page: Page
-    readonly formsLayoutMenuItem: Locator
-    readonly datePickerMenuItem: Locator
-    readonly smartTableMenuItem: Locator
-    readonly toastrMenuItem: Locator
-    readonly toolTipMenuItem: Locator
 
     constructor(page: Page){
         this.page = page
-        this.formsLayoutMenuItem = page.getByText('Form Layouts')
-        this.datePickerMenuItem = page.getByText('Datepicker')
-        this.smartTableMenuItem = page.getByText('Smart Table')
-        this.toastrMenuItem = page.getByText('Toastr')
-        this. toolTipMenuItem = page.getByText('Tooltip')
     }
 
     async formLayoutsPage(){
         await this.selectGroupMenuItem('Forms')
-        await this.formsLayoutMenuItem.click()
+        await this.page.getByText('Form Layouts').click()
     }
 
     async datePickerPage(){
         await this.selectGroupMenuItem('Forms')
-        await this.datePickerMenuItem.click()
+        await this.page.getByText('Datepicker').click()
     }
 
     async smartTablePage(){
         await this.selectGroupMenuItem('Tables & Data')
-        await this.smartTableMenuItem.click()
+        await this.page.getByText('Smart Table').click()
     }
 
     async toastrPAge(){
         await this.selectGroupMenuItem('Modal & Overlays')
-        await this.toastrMenuItem.click()
+        await this.page.getByText('Toastr').click()
     }
 
     async toolTipPage(){
         await this.selectGroupMenuItem('Modal & Overlays')
-        await this.toolTipMenuItem.click()
+        await this.page.getByText('Tooltip').click()
     }
 
     private async selectGroupMenuItem(groupItemTitle: string){

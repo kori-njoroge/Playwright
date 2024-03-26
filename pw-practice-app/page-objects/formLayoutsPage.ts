7f162ccdfb086566c436e9f64847523bb4e6a802
Kori Njoroge <korinjoroge63@gmail.com>
2024-03-26 12:32:51 +0300

    creating parameterized methods

---
+++ pw-practice-app/page-objects/formLayoutsPage.ts
@@ -0,0 +1,31 @@
+import { Page } from "@playwright/test";
+
+export class FormLayoutsPage{
+    private readonly page: Page
+
+    constructor(page: Page){
+        this.page = page;
+    }
+
+    async submitUsingTheGrid(email: string, password: string, option: string){
+        const usingTheGridForm = this.page.locator('nb-card', { hasText: "Using the Grid" })
+        await usingTheGridForm.getByRole('textbox', { name: "Email" }).fill(email)
+        await usingTheGridForm.getByRole('textbox', { name: "Password" }).fill(password)
+        await usingTheGridForm.getByRole('radio', { name: option }).check({force: true})
+        await usingTheGridForm.getByRole('button').click()
+    }
+    /**
+     * this method fill out the inline orm with user details
+     * @param name - should be fisrt and last name
+     * @param email - valid user email
+     * @param rememberMe - true or false
+     */
+    async submitInlineForm(name: string, email: string, rememberMe: boolean){
+        const inlineForm = this.page.locator('nb-card', {hasText: 'Inline Form'});
+        await inlineForm.getByPlaceholder('Jane Doe').fill(name);
+        await inlineForm.getByPlaceholder('Email').fill(email)
+        if(rememberMe)
+            await inlineForm.getByRole('checkbox').check({force: true})
+        await inlineForm.getByRole('button').click()
+    }
+}

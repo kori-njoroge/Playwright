26bb6e4b68ba313a21c033d319004a9bbfe34fd9
Kori Njoroge <korinjoroge63@gmail.com>
2024-03-26 13:14:40 +0300

    date picker with a range

--- pw-practice-app/tests/usePageObjects.spec.ts
+++ pw-practice-app/tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import {test} from "@playwright/test"
 import { NavigatioPage } from "../page-objects/navigatioPage"
 import {FormLayoutsPage} from '../page-objects/formLayoutsPage'
+import { DatePickerPage } from "../page-objects/datePickerPage"
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200')
@@ -19,8 +20,13 @@ test("Navigate to form page", async({page})=>{
 test('Parameterized Methods', async({page})=>{
     const navigateTo = new NavigatioPage(page)
     const onFormLayoutsPage = new FormLayoutsPage(page)
+    const onDatePickerPage = new DatePickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGrid('kori@test.com','Password','Option 1')
     await onFormLayoutsPage.submitInlineForm('Gideon Kori','Kori@test.com',true)
+
+    await navigateTo.datePickerPage()
+    await onDatePickerPage.selectCommonDatePickerDateFromToday(200)
+    // await onDatePickerPage.selectDatePickerWithRangeFromToday(6,20)
 })

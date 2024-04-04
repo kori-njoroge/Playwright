d0bde92bba7ea7f8e1ceb5b05ff91d2b17d973dc
Kori Njoroge <korinjoroge63@gmail.com>
2024-04-04 13:52:19 +0300

    Replacing the datt a withing the API

--- pw-api-practice/tests/workingWithAPI.spec.ts
+++ pw-api-practice/tests/workingWithAPI.spec.ts
@@ -11,6 +11,18 @@ test.beforeEach(async({page})=>{
     body: JSON.stringify(tagList)
   })
 })
+
+await page.route('*/**/api/articles*', async route =>{
+  const response = await route.fetch()
+  const responseBody = await response.json()
+  responseBody.articles[0].title = "This is a test title"
+  responseBody.articles[0].description = "This is a description"
+  
+  await route.fulfill({
+    body: JSON.stringify(responseBody)
+  });
+})
+
 await page.goto("https://conduit.bondaracademy.com/")
 await page.waitForTimeout(500)
 })
@@ -18,6 +30,9 @@ await page.waitForTimeout(500)
 test('has title', async ({ page }) => {
   console.log('Here we gooo')
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a description')
+  // is you do not have assertions please enter a temporary timeout so that playwright can complete execution before ccompleting the test
 });
 
 
